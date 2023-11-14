443815e4d9e6dd348d2ce6206d18a0d346f0d1b2
rick624 <rick624@so-net.net.tw>
2023-11-14 16:20:13 +0800

    function design, toast, dialog

--- app/src/main/java/com/rick/guess/MainActivity2.kt
+++ app/src/main/java/com/rick/guess/MainActivity2.kt
@@ -2,10 +2,44 @@ package com.rick.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity2 : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+
+    lateinit var edNumber: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "secret: "+ secretNumber.secret)
+        findViews()
+    }
+
+    private fun findViews() {
+        edNumber = findViewById(R.id.ed_number)
+    }
+
+    fun check(view:View){
+        val n = edNumber.text.toString().toInt()
+        println("number: $n")
+        Log.d("MainActivity", "number:" + n)
+
+        val diff = secretNumber.validate(n)
+        var message = "Yes, you got it"
+        if (diff < 0){
+            message = "Bigger"
+        } else if (diff > 0){
+            message = "Smaller"
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("ok", null)
+            .show()
     }
 }
